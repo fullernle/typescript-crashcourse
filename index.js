7060c5185947b80d862b9d3e7a8e73f8dfc8d23b
lefuller <fullernle@gmail.com>
2022-01-06 14:23:32 -0800

    installs tsc, creates initial tsconfig file and compiles .ts file via tsc

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+// let id = 5; // standard js, but typescript with implicitly set it as a Number
+let id = 5; // TypeScript explicit declaration of number type.
+// Watch the file using tsc --watch index
+// tsc --init will create config file
---
+++ index.ts
@@ -0,0 +1,6 @@
+// let id = 5; // standard js, but typescript with implicitly set it as a Number
+let id: number = 5; // TypeScript explicit declaration of number type.
+
+// Watch the file using tsc --watch index
+// tsc --init will create config file
+// Running tsc without a file afterwards will just compile any typescript file in the project

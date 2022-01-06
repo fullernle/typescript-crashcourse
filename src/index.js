d83f209210999a679bb31482c9321a3f3de7e613
lefuller <fullernle@gmail.com>
2022-01-06 14:41:02 -0800

    goes over initial three basic data types and any type

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 // Watch the file using tsc --watch index
 // tsc --init will create config file
 // Running tsc without a file afterwards will just compile any typescript file in the project
-
 // let id = 5; // standard js, but typescript with implicitly set it as a Number
 // let id: number = 5; // TypeScript explicit declaration of number type.
-
 // Basic Types 
-let id: number = 5;
-let company: string = 'Fuller'
-let isPublished: boolean = true 
-let x: any = 'Hello'
-
+var id = 5;
+var company = 'Fuller';
+var isPublished = true;
+var x = 'Hello';
 x = true; // This won't cause an error because we set it to be anything.
-
 // Can also initialize and assign letter
-let age: number;
+var age;
 age = 5;
-
-

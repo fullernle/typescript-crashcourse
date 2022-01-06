6f819f1f3d4f72b250bc1d51907d626a811e30bc
lefuller <fullernle@gmail.com>
2022-01-06 14:28:33 -0800

    implements basic src and dist file structure using tsconfig

--- dist/index.js
+++ dist/index.js
@@ -3,3 +3,5 @@
 let id = 5; // TypeScript explicit declaration of number type.
 // Watch the file using tsc --watch index
 // tsc --init will create config file
+// Running tsc without a file afterwards will just compile any typescript file in the project
+console.log({ id });
